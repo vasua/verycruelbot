9f80e43e4cf960db58c8fd8282d5d6f8f23a7a55
Andrey Shimon <vsvasua@gmail.com>
2015-11-26 01:11:35 +0200

    Add requirements and try to specify port

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import random
 import datetime
 """
 import telepot
+import os
+from bottle import run
 from telepot.delegate import per_chat_id, create_open
 
 
@@ -13,8 +15,16 @@ class VeryCruel(telepot.helper.ChatHandler):
     def __init__(self, seed_tuple, timeout):
         super(VeryCruel, self).__init__(seed_tuple,timeout)
 
-    def reply_to_serjant(self,m):
-        self.sender.sendMessage('Сообщение получено от %s c id %s' % (m.from_, m.from_.id))
+    def reply_to_serjant(self, m):
+        if m.from_.first_name == 'Сержант':
+            self.sender.sendMessage('ИДИ ФПИЗДУ ПИДАРАС', reply_to_message_id=m.message_id)
+            return True
+        else:
+            return False
+
+    def reply_to_badgay(self, content_type, m):
+        if content_type == 'text' and 'хуй' in m.text:
+            self.sender.sendMessage('НЕ МАТЕРИСЬ, ПИДАРАС!', reply_to_message_id=m.message_id)
 
     def on_message(self, msg):
         content_type, chat_type, chat_id = telepot.glance2(msg)
@@ -22,16 +32,23 @@ class VeryCruel(telepot.helper.ChatHandler):
 
         if chat_id < 0:
             # public chat
-            self.reply_to_serjant(m)
+            if not self.reply_to_serjant(m):
+                self.reply_to_badgay(content_type,m)
         else:
             # private conversation
-            if content_type == 'text':
-                if 'хуй' in m.text:
-                    self.sender.sendMessage('НЕ МАТЕРИСЬ, ПИДАРАС!', reply_to_message_id=m.message_id)
+            self.reply_to_badgay(content_type, m)
+
+    """
+    def on_close(self, exception):
+        if isinstance(exception, telepot.helper.WaitTooLong):
+            bot.notifyOnMessage(run_forever=True)
+    """
 
 TOKEN = '148865285:AAHvwDHJGVrSzEGJ_ToGUxk1RWclvX2L_W4'
 
 bot = telepot.DelegatorBot(TOKEN, [
-    (per_chat_id(), create_open(VeryCruel, timeout=15)),
+    (per_chat_id(), create_open(VeryCruel, timeout=1)),
 ])
 bot.notifyOnMessage(run_forever=True)
+
+run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
