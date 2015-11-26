fcc882f2fb5b5dda26e5181e887f0b6f1ddee32a
Andrey Shimon <vsvasua@gmail.com>
2015-11-26 03:30:05 +0200

    Bot cant reply bots because of telegram api

--- app.py
+++ app.py
@@ -15,11 +15,7 @@ class VeryCruel(telepot.helper.ChatHandler):
     def __init__(self, seed_tuple, timeout):
         super(VeryCruel, self).__init__(seed_tuple,timeout)
 
-    def reply_to_serjant(self, content_type, m):
-
-        # self.sender.sendMessage('А это у нас был %s' % m.from_.first_name)
-
-        # if m.from_.first_name == 'Сержант':
+    def reply_to_kek(self, content_type, m):
         if content_type == 'text' and 'кек' in m.text:
             self.sender.sendMessage('ИДИ ФПИЗДУ ПИДАРАС', reply_to_message_id=m.message_id)
             return True
@@ -31,7 +27,6 @@ class VeryCruel(telepot.helper.ChatHandler):
             self.sender.sendMessage('НЕ МАТЕРИСЬ, ПИДАРАС!', reply_to_message_id=m.message_id)
 
     def on_message(self, msg):
-        self.sender.sendMessage('here')
         content_type, chat_type, chat_id = telepot.glance2(msg)
         m = telepot.namedtuple(msg, 'Message')
 
